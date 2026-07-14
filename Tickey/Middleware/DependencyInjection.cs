9ce4986012ed169b6fb23276390a33785070dd10
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2026-07-14 11:49:52 +0200

    Configured services to clean up program.cs

---
+++ Tickey/Middleware/DependencyInjection.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Tickey.Sevices;
+
+namespace Tickey.Middleware;
+
+public static class DependencyInjection 
+{
+    public static IServiceCollection AddErrorHandlingAndLogs(this IServiceCollection services)
+    {
+        services.AddProblemDetails();
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddLogging();
+
+        return services;
+    }
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHealthChecks().AddNpgSql(configuration.GetConnectionString("TickeyDB")!);
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("TickeyDB")));
+
+        return services;
+    }
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = configuration["AppSettings:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = configuration["AppSettings:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AppSettings:Token"]!)),
+            ValidateIssuerSigningKey = true,
+        });
+
+        services.AddAuthorizationBuilder()
+                .AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"))
+                .AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+        return services;
+    }
+
+    public static IServiceCollection AddRateLimiting(this IServiceCollection services)
+    {
+        services.AddRateLimiter(options =>
+        {
+            options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+            options.AddFixedWindowLimiter("Fixed", opt =>
+            {
+                opt.PermitLimit = 10;
+                opt.Window = TimeSpan.FromSeconds(30);
+            });
+        });
+
+        return services;
+
+    }
+
+    public static IServiceCollection DependentServices(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<TokenService>();
+        services.AddScoped<AuthService>();
+
+        return services;
+    }
+}
--- Tickey/Program.cs
+++ Tickey/Program.cs
@@ -1,65 +1,29 @@
 using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.RateLimiting;
 using Tickey.Endpoints;
 using Tickey.Middleware;
 using Tickey.Sevices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// metric, error, logging
-builder.Services.AddProblemDetails();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+// metric, logging
+builder.Services.AddErrorHandlingAndLogs();
+
 builder.Services.AddOpenApi();
-builder.Services.AddLogging();
 
 // db
-builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("TickeyDB")!);
+builder.Services.AddDatabase(builder.Configuration);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("TickeyDB")));
+//auth 
+builder.Services.AddAuth(builder.Configuration);
 
-//auth config
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["AppSettings:Issuer"],
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["AppSettings:Audience"],
-        ValidateLifetime = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]!)),
-        ValidateIssuerSigningKey = true,
-    });
-
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"))
-    .AddPolicy("UserOnly", policy => policy.RequireRole("User"));
-
-builder.Services.AddRateLimiter(options =>
-{ 
-    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-
-    options.AddFixedWindowLimiter("Fixed", opt =>
-    {
-        opt.PermitLimit = 10;
-        opt.Window = TimeSpan.FromSeconds(30);     
-    });
-
-});
+//rate limiting
+builder.Services.AddRateLimiting();
 
 // services
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<TokenService>();
-builder.Services.AddScoped<AuthService>();
+builder.Services.DependentServices();
 
 var app = builder.Build();
 
@@ -86,6 +50,6 @@ app.MapHealthChecks("/health", new HealthCheckOptions
     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
 });
 
-// ratelimiting middleware, aspire for metrics. Work more on events and ticket models. Add tests and more methods.
+//  aspire for metrics. Work more on events and ticket models. Add tests and more methods.
 
 app.Run();
